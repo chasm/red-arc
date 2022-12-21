0a55eaf5895d1e92d099e324103e3989f651d7e6
Charles F. Munat <charles.munat@gmail.com>
2022-12-21 20:12:45 +1300

    chore: format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import compress from "astro-compress"
-import { defineConfig } from "astro/config"
 import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
+import compress from "astro-compress"
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
